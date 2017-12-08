f3022283bf82f5f5e4474afc0ca0067f9e07f7cd
liberty8374 <liberty8374@yandex.ru>
2017-12-08 05:35:09 +0300

    better table using vue-tables-2 and bootstrap

--- episodes.js
+++ episodes.js
@@ -1,10 +1,39 @@
 let data = {
     episodes: [],
-    show_stories: false
+    show_stories: false,
+    default_table: false,
+    loaded: false,
+    columns: ['story', 'episode', 'id'],
+    tableOptions: {
+        headings: {
+            'story': 'Сторимейз',
+            'episode': 'Эпизод',
+            'id': 'Ссылка'
+        },
+        highlightMatches: true,
+        perPageValues: [10, 30, 100, 300, 1000, 10000],
+        perPage: 300,
+        pagination: {
+            dropdown: true
+        },
+        texts: {
+            count: "Записи с {from} по {to} из {count}",
+            filter: "Поиск:",
+            filterPlaceholder: "запрос",
+            limit: "Записи на странице:",
+            page: "Страница:",
+            noResults: "Нет подходящих записей",
+            filterBy: "Фильтр по {column}",
+            loading: 'Загрузка...',
+            defaultOption: 'Выбрать {column}'
+        }
+    }
 };
 
+Vue.use(VueTables.ClientTable);
+
 let app = new Vue({
-    el: '#episodes_table',
+    el: '#app',
     data: data,
     methods: {
         filtered() {
@@ -19,6 +48,7 @@ let app = new Vue({
 
 $.getJSON('/episodes.json').then(function (json) {
     data.episodes = json.episodes;
+    data.loaded = true;
 }, function (err) {
     console.error(err);
 });
